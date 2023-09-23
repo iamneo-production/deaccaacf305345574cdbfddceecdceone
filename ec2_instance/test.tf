861d5a60578e78d1b92103490bccbbb86babc5c1
Iamneo <techadmin@iamneo.ai>
2023-09-23 06:37:03 +0000

    Backup Commit

--- ec2_instance/test.tf
+++ ec2_instance/test.tf
@@ -1,22 +1,8 @@
 terraform {
     required_providers {
       aws = {
-          source = "hashicorp/aws"
+          so
       }
     }
-}
-
-provider "aws" {
-    region = "us-east-2"
-    access_key = "AKIAZIV2HI7BH7BHRSVG"
-    secret_key = "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"
-}
-
-resource "aws_instance" "ec2_instance" {
-    ami = ""
-    instace_type = ""
-}
-
-output "public_ip" {
-    value = aws_instance.ec2_instance.public_ip
+    
 }
